789c5d3701d69a81ab2a621d34b6b71fc9f21ec5
Shibraj <shibrajdeb456@gmail.com>
2023-07-18 18:50:08 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+const Home = ()=>
+{
+  return (
+   
+    <section className="flex-start flex-col paddings mb-16">
+      <h1>Catagories</h1>
+      <h1>post</h1>
+      <h1>loadmore</h1>
+    </section>
+    
+  )
+}
+
+export default Home
